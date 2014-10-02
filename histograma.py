faefc4d7cb2375b33ba37d45878a1ede435a1b33
Mikhail Pedrosa <mikhailj.pedrosa@gmail.com>
2014-10-02 11:10:02 -0300

    Update histograma.py
    Conserto no Codigo

--- histograma.py
+++ histograma.py
@@ -4,7 +4,7 @@ __description__ = 'Operações com Imagens - Histograma, Operações Pontuais, L
 __version__ = '0.1'
 __date__ = '15/09/2014'
 
-<<<<<<< HEAD
+
 import cv
 from PIL import Image
 from pylab import *
@@ -17,7 +17,7 @@ from PIL import Image
 from pylab import *
 
 
->>>>>>> 438f686174139119fd8542cb59283a0ca3901d1a
+
 def gerarHist(imagem1, imagem2):
 
     matriz1 = np.asarray(imagem1.convert('L'))
@@ -25,7 +25,6 @@ def gerarHist(imagem1, imagem2):
 
     #Visualização do Histograma (FotoCinza)
     plt.hist(matriz1.flatten(), color='green')
-<<<<<<< HEAD
     plt.title('Histograma')
     plt.xlabel('Pixel')
     plt.ylabel('Ocorrencias')
@@ -33,14 +32,12 @@ def gerarHist(imagem1, imagem2):
 
     #Visualização do Histograma (FotoCinza2)
     plt.hist(matriz2.flatten(), color='red')
-=======
->>>>>>> 438f686174139119fd8542cb59283a0ca3901d1a
     plt.title('Histograma')
     plt.xlabel('Pixel')
     plt.ylabel('Ocorrencias')
     plt.show()
 
-<<<<<<< HEAD
+
 def rotacao(imagem1, imagem2):
 
     #Rotação da Imagem 90 graus e 180 graus
@@ -49,32 +46,15 @@ def rotacao(imagem1, imagem2):
 
     imagem2.transpose(Image.ROTATE_90).show(title='Imagem 90')
     imagem2.transpose(Image.ROTATE_180).show(title='Imagem 180')
-=======
-    #Visualização do Histograma (FotoCinza2)
-    plt.hist(matriz2.flatten(), color='red')
-    plt.title('Histograma')
-    plt.xlabel('Pixel')
-    plt.ylabel('Ocorrencias')
-    plt.show()
 
-def rotacao(imagem1, imagem2):
-
-    #Rotação da Imagem 90 graus e 180 graus
-    imagem1.rotate(90).show(title = 'Imagem 90')
-    imagem2.rotate(180).show(title = 'Imagem 180')
->>>>>>> 438f686174139119fd8542cb59283a0ca3901d1a
 
 def espelhamento(imagem1, imagem2):
 
     imagem1.transpose(Image.FLIP_LEFT_RIGHT).show()
-<<<<<<< HEAD
     imagem1.transpose(Image.FLIP_TOP_BOTTOM).show()
 
     imagem2.transpose(Image.FLIP_LEFT_RIGHT).show()
     imagem2.transpose(Image.FLIP_TOP_BOTTOM).show()
-=======
-    imagem2.transpose(Image.FLIP_LEFT_RIGHT).show()
->>>>>>> 438f686174139119fd8542cb59283a0ca3901d1a
 
 if __name__ == '__main__':
 
@@ -92,13 +72,8 @@ if __name__ == '__main__':
     #Operações Lógicas (And, Ou, Xor)
 
     #Operações Locais
-<<<<<<< HEAD
 
 
-=======
-
-
->>>>>>> 438f686174139119fd8542cb59283a0ca3901d1a
     #Transformações Geométricas
     #Translação
     # img = imread('fotocinza.jpg',0)
@@ -113,23 +88,13 @@ if __name__ == '__main__':
     # cv.destroyAllWindows()
 
     #Rotação
-<<<<<<< HEAD
     #rotacao(imagem1, imagem2)
-=======
-    rotacao(imagem1, imagem2)
->>>>>>> 438f686174139119fd8542cb59283a0ca3901d1a
 
     #Escala
 
 
     #Espelhamento ou Reflexão
     espelhamento(imagem1, imagem2)
-<<<<<<< HEAD
 
     #r, g, b = imagem.split()
     #im = Image.merge("RGB", (b, g, r))
-=======
-    
-    #r, g, b = imagem.split()
-    #im = Image.merge("RGB", (b, g, r))
->>>>>>> 438f686174139119fd8542cb59283a0ca3901d1a
