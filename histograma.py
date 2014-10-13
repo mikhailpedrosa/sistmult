68f0a5aa600fcf4ddd662883592b5b08f320b1f1
Mikhail Oliveira <mikhail.oliveira@funceme.br>
2014-10-13 09:04:23 -0300

    Correção de algumas funções e outros ajustes

--- histograma.py
+++ histograma.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-__author__ = 'Mikhail Pedrosa <mikhailj.pedrosa@gmail.com> e Tiago Alves <tiagoinformativa@gmail.com >'
+__author__ = 'Mikhail Pedrosa <mikhail.jose@hotmail.com> e Tiago AlvesTiago Alves <tiagoinformativa@gmail.com >'
 __description__ = 'Operações com Imagens - Histograma, Operações Pontuais, Lógicas, Aritméticas, Translação, Rotação e Escala'
-__version__ = '1.0'
+__version__ = '0.1'
 __date__ = '15/09/2014'
 
 import matplotlib.pyplot as plt
@@ -10,34 +10,47 @@ import PIL
 from PIL import Image
 from pylab import *
 
+#Bibliotecas Usadas - matplotlib, numpy, pillow
+#Python 2.7
 
-def histograma(imagem1, imagem2):
-    imagem_matriz1 = np.asarray(imagem1.convert('L'))
-    imagem_matriz2 = np.asarray(imagem2.convert('L'))
+def histogram(imagem1, imagem2):
+    matriz1 = np.asarray(imagem1.convert('L'))
+    matriz2 = np.asarray(imagem2.convert('L'))
 
     # Visualização do Histograma (FotoCinza)
-    plt.hist(imagem_matriz1.flatten(), color='green')
+    plt.hist(matriz1.flatten(), color='green')
     plt.title('Histograma')
     plt.xlabel('Pixel')
     plt.ylabel('Ocorrencias')
     plt.show()
 
     # Visualização do Histograma (FotoCinza2)
-    plt.hist(imagem_matriz2.flatten(), color='red')
+    plt.hist(matriz2.flatten(), color='red')
     plt.title('Histograma')
     plt.xlabel('Pixel')
     plt.ylabel('Ocorrencias')
     plt.show()
 
 
-def rotacao(imagem):
+def rotacao(imagem1):
+    imagem1 = Image.open("foto1.jpg")
     # Rotação da Imagem 90 graus e 180 graus
-    return imagem.transpose(Image.ROTATE_90).save("rotacao_90"), imagem.transpose(Image.ROTATE_180).save("rotacao_180")
+    rot_90 = imagem1.transpose(Image.ROTATE_90)
+    rot_90 = rot_90.save("rotacao_90.png")
+    rot_180 = imagem1.transpose(Image.ROTATE_180)
+    rot_180 = rot_180.save("rotacao_180.png")
+
+    return rot_90, rot_180
 
 
-def espelhamento(imagem):
-    # Espelhamento da Imagem Esq - Dir / Cima - Baixo
-    return imagem.transpose(Image.FLIP_LEFT_RIGHT).save("espelhamento_left_right.png"), imagem.transpose(Image.FLIP_TOP_BOTTOM).save("espelhamento_top.png")
+def espelhamento(imagem1):
+    imagem1 = Image.open("foto1.jpg")
+    left_right = imagem1.transpose(Image.FLIP_LEFT_RIGHT)
+    left_right = left_right.save("espelhamento_left_right.png")
+    top = imagem1.transpose(Image.FLIP_TOP_BOTTOM)
+    top = top.save("espelhamento_top.png")
+
+    return left_right, top
 
 
 def soma(imagem1, imagem2):
@@ -131,7 +144,7 @@ def div(imagem1, imagem2):
                 green_im1 /= green_im2
                 blue_im1 /= blue_im2
             except ZeroDivisionError:
-                print "Você não pode dividir por zero."
+                print #"You can't divide by zero, you're silly."
 
             #Junção dos Pixels das Imagens
             imagem1.putpixel((x, y), (red_im1, green_im1, blue_im1))
@@ -158,6 +171,7 @@ def E(imagem1, imagem2):
             green_im1 and green_im2
             blue_im1 and blue_im2
 
+
             #Junção dos Pixels das Imagens
             imagem1.putpixel((x, y), (red_im1, green_im1, blue_im1))
 
@@ -182,6 +196,7 @@ def OU(imagem1, imagem2):
             red_im1 or red_im2
             green_im1 or green_im2
             blue_im1 or blue_im2
+
 
             #Junção dos Pixels das Imagens
             imagem1.putpixel((x, y), (red_im1, green_im1, blue_im1))
@@ -208,37 +223,33 @@ def XOR(imagem1, imagem2):
             green_im1 ^ green_im2
             blue_im1 ^ blue_im2
 
+
             #Junção dos Pixels das Imagens
             imagem1.putpixel((x, y), (red_im1, green_im1, blue_im1))
 
     return imagem1.save('xor.png')
 
 
-def escala(imagem):
-    #Largura Base
+def escala(imagem1):
+    #Largura da Base
     basewidth=300
-    #Porcentagem da Largura
-    wpercent = (basewidth/float(imagem.size[0]))
+    #Porcento da Altura
+    wpercent = (basewidth/float(imagem1.size[0]))
     #Tamanho da Altura
-    hsize = int((float(imagem.size[1])*float(wpercent)))
-    imagem_esc_menor = imagem.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
-    imagem_esc_maior = imagem.resize((1200,800), PIL.Image.ANTIALIAS)
+    hsize = int((float(imagem1.size[1])*float(wpercent)))
+    imagem1 = imagem1.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
+    imagem_maior = imagem1.resize((1500, 1000))
 
-    return imagem_esc_menor.save('escala_menor.jpg'), imagem_esc_maior.save('escala_maior.jpg')
-
+    return imagem1.save('escala_menor.png'), imagem_maior.save("escala_maior.png")
 
 def translacao():
-    #Abrir Imagem
     imagem = Image.open("foto.png")
-
     #Tamanho Imagem - Largura e Altura
     lar = imagem.size[0]
     alt = imagem.size[1]
-    x_loc = 200
-    y_loc = 200
-
+    x_loc = 100
+    y_loc = 150
     imagem_matriz = np.asarray(imagem.convert('RGB'))
-
     for x in range(lar):
         for y in range(alt):
             if x >= x_loc and y >= y_loc:
@@ -247,19 +258,19 @@ def translacao():
                 imagem.putpixel((x,y), (imagem_matriz[xo,yo][0],imagem_matriz[xo,yo][1],imagem_matriz[xo,yo][2]))
             else:
                 imagem.putpixel((x,y), (255, 255, 255, 255))
-    
+
     return imagem.save("translate.png")
 
-
 if __name__ == '__main__':
-    #Leitura das Imagens e Conversão para RGB
+    # Leitura das Imagens e Conversão para RGB
     imagem1 = Image.open("foto1.jpg").convert('RGB')
     imagem2 = Image.open("foto2.jpg").convert('RGB')
 
     #Histograma
-    histograma(imagem1, imagem2)
+    histogram(imagem1, imagem2)
 
-    #Operações Pontuais - Aritméticas (Soma, Subtração, Multiplicação, Divisão)
+    #Operações Pontuais
+    #Operações Aritméticas (Soma, Subtração, Multiplicação, Divisão)
     soma(imagem1, imagem2)
     subt(imagem1, imagem2)
     mult(imagem1, imagem2)
@@ -272,7 +283,6 @@ if __name__ == '__main__':
 
     #Operações Locais
 
-
     #Transformações Geométricas
     translacao()
 
@@ -284,3 +294,4 @@ if __name__ == '__main__':
 
     #Espelhamento ou Reflexão
     espelhamento(imagem1)
+
