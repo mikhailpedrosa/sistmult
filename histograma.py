3a017a7a574a5f00b1702fd505d75d2aa8f4626a
Mikhail Oliveira <mikhail.oliveira@funceme.br>
2014-10-13 10:56:43 -0300

    Adicionando Funções desfoque_alisamento, suavizacao e afinacao

--- histograma.py
+++ histograma.py
@@ -4,11 +4,15 @@ __description__ = 'Operações com Imagens - Histograma, Operações Pontuais, L
 __version__ = '0.1'
 __date__ = '15/09/2014'
 
+
+from scipy import ndimage
+from pylab import *
+from PIL import Image
+import PIL
+import scipy
 import matplotlib.pyplot as plt
 import numpy as np
-import PIL
-from PIL import Image
-from pylab import *
+
 
 #Bibliotecas Usadas - matplotlib, numpy, pillow
 #Python 2.7
@@ -237,10 +241,12 @@ def escala(imagem1):
     wpercent = (basewidth/float(imagem1.size[0]))
     #Tamanho da Altura
     hsize = int((float(imagem1.size[1])*float(wpercent)))
-    imagem1 = imagem1.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
-    imagem_maior = imagem1.resize((1500, 1000))
+    imagem_menor = imagem1.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
+    lar=1500
+    alt = 1000
+    imagem_maior = imagem1.resize((lar,alt))
 
-    return imagem1.save('escala_menor.png'), imagem_maior.save("escala_maior.png")
+    return imagem_menor.save('escala_menor.png'), imagem_maior.save("escala_maior.png")
 
 def translacao():
     imagem = Image.open("foto.png")
@@ -261,37 +267,112 @@ def translacao():
 
     return imagem.save("translate.png")
 
+
+def desfoque_alisamento(imagem):
+
+    blurred_lena = ndimage.gaussian_filter(imagem, sigma=3)
+    very_blurred = ndimage.gaussian_filter(imagem, sigma=5)
+    local_mean = ndimage.uniform_filter(imagem, size=11)
+
+    plt.figure(figsize=(9, 3))
+    plt.subplot(131)
+    plt.imshow(blurred_lena, cmap=plt.cm.gray)
+    plt.axis('off')
+    plt.subplot(132)
+    plt.imshow(very_blurred, cmap=plt.cm.gray)
+    plt.axis('off')
+    plt.subplot(133)
+    plt.imshow(local_mean, cmap=plt.cm.gray)
+    plt.axis('off')
+
+    plt.subplots_adjust(wspace=0, hspace=0., top=0.99, bottom=0.01,
+                        left=0.01, right=0.99)
+    plt.show()
+
+def afinacao(imagem):
+
+    blurred_imagem = ndimage.gaussian_filter(imagem, 3)
+    filter_blurred_imagem = ndimage.gaussian_filter(blurred_imagem, 1)
+
+    alpha = 30
+    sharpened = blurred_imagem + alpha * (blurred_imagem - filter_blurred_imagem)
+
+    plt.figure(figsize=(12, 4))
+
+    plt.subplot(131)
+    plt.imshow(imagem, cmap=plt.cm.gray)
+    plt.axis('off')
+    plt.subplot(132)
+    plt.imshow(blurred_imagem, cmap=plt.cm.gray)
+    plt.axis('off')
+    plt.subplot(133)
+    plt.imshow(sharpened, cmap=plt.cm.gray)
+    plt.axis('off')
+
+    plt.show()
+
+def suavizacao():
+
+    imagem = scipy.misc.lena()
+    imagem = imagem[230:290, 220:320]
+
+    noisy = imagem + 0.4*imagem.std()*np.random.random(imagem.shape)
+    gauss_denoised = ndimage.gaussian_filter(noisy, 2)
+    med_denoised = ndimage.median_filter(noisy, 3)
+
+
+    plt.figure(figsize=(12,2.8))
+    plt.subplot(131)
+    plt.imshow(noisy, cmap=plt.cm.gray, vmin=40, vmax=220)
+    plt.axis('off')
+    plt.title('Noisy', fontsize=20)
+    plt.subplot(132)
+    plt.imshow(gauss_denoised, cmap=plt.cm.gray, vmin=40, vmax=220)
+    plt.axis('off')
+    plt.title('Gaussian filter', fontsize=20)
+    plt.subplot(133)
+    plt.imshow(med_denoised, cmap=plt.cm.gray, vmin=40, vmax=220)
+    plt.axis('off')
+    plt.title('Median filter', fontsize=20)
+    plt.subplots_adjust(wspace=0.02, hspace=0.02, top=0.9, bottom=0, left=0,
+                        right=1)
+    plt.show()
+
+
 if __name__ == '__main__':
     # Leitura das Imagens e Conversão para RGB
     imagem1 = Image.open("foto1.jpg").convert('RGB')
     imagem2 = Image.open("foto2.jpg").convert('RGB')
 
     #Histograma
-    histogram(imagem1, imagem2)
+    #histogram(imagem1, imagem2)
 
     #Operações Pontuais
     #Operações Aritméticas (Soma, Subtração, Multiplicação, Divisão)
-    soma(imagem1, imagem2)
-    subt(imagem1, imagem2)
-    mult(imagem1, imagem2)
-    div(imagem1, imagem2)
+    #soma(imagem1, imagem2)
+    #subt(imagem1, imagem2)
+    #mult(imagem1, imagem2)
+    #div(imagem1, imagem2)
 
     #Operações Lógicas (And, Ou, Xor)
-    E(imagem1, imagem2)
-    OU(imagem1, imagem2)
-    XOR(imagem1, imagem2)
+    #E(imagem1, imagem2)
+    #OU(imagem1, imagem2)
+    #XOR(imagem1, imagem2)
 
     #Operações Locais
+    #desfoque_alisamento(imagem1)
+    afinacao(imagem1)
+    suavizacao()
 
     #Transformações Geométricas
-    translacao()
+    #translacao()
 
     #Rotação
-    rotacao(imagem1)
+    #rotacao(imagem1)
 
     #Escala
-    escala(imagem1)
+    #escala(imagem1)
 
     #Espelhamento ou Reflexão
-    espelhamento(imagem1)
+    #espelhamento(imagem1)
 
